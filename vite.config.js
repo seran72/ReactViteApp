d002f4ec10ff0ae5a7315cf21efff0ea634fc015
unknown <Sdiriba33@gmail.com>
2023-09-23 00:45:18 -0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: false,
-    port: 5183, // you can replace this port with any port
+   // you can replace this port with any port
   }
   })
 
